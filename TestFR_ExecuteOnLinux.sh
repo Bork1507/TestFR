be3f7636f41398d0604bba8b5e16b8aa3b167ef4
Bork <bork2@mail.ru>
2016-06-09 10:24:49 +0300

    Example run program on OS Windows

---
+++ TestFR_ExecuteOnLinux.sh
@@ -0,0 +1,5 @@
+md bin
+echo "Compile TestFR..."
+javac -d ./bin -sourcepath ./src -classpath ./lib/sqlite-jdbc-3.7.2.jar:./lib/crunchify.jar:./lib/jssc.jar ./src/TestFR.java
+echo "Run TestFR..."
+java -classpath .:./bin:./lib/sqlite-jdbc-3.7.2.jar:./lib/crunchify.jar:./lib/jssc.jar TestFR
