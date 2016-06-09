7e69adbdfc30bf515b757a0be77400be9ecdcd07
Bork <bork2@mail.ru>
2016-06-09 10:40:56 +0300

    Example run program on OS Linux

--- TestFR_ExecuteOnLinux.sh
+++ TestFR_ExecuteOnLinux.sh
@@ -1,6 +1,6 @@
-md bin
+mkdir bin
 echo "Compile TestFR..."
 javac -d ./bin -sourcepath ./src -classpath ./lib/sqlite-jdbc-3.7.2.jar:./lib/crunchify.jar:./lib/jssc.jar ./src/TestFR.java
 echo "Run TestFR..."
 java -classpath .:./bin:./lib/sqlite-jdbc-3.7.2.jar:./lib/crunchify.jar:./lib/jssc.jar TestFR
-pause
+read -p "Press any key..."
