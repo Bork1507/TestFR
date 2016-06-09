4153d71c1abac7efa8fbd8f5f3b70fdef8b186e0
Bork <bork2@mail.ru>
2016-06-09 10:40:41 +0300

    Example run program on OS Windows

--- TestFR_ExecuteOnWindows.bat
+++ TestFR_ExecuteOnWindows.bat
@@ -3,3 +3,4 @@ echo "Compile TestFR..."
 javac -d bin -sourcepath src -classpath lib\sqlite-jdbc-3.7.2.jar:lib\crunchify.jar:lib\jssc.jar src\TestFR.java
 echo "Run TestFR..."
 java -classpath bin;lib\sqlite-jdbc-3.7.2.jar;lib\crunchify.jar;lib\jssc.jar TestFR
+pause
